74c823dd330b2fa941c4e0e9e18b730393b48de7
Ttibsi <ashisbitt@icloud.com>
2022-07-07 12:54:02 +0100

    Set up and initialise pre-commit

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 
 /*
-* A PPM file is a type of file that represents an image by specifying
-* every line of pixels, as well as some data at the start that informs the 
-* renderer how to display the image. 
-*
-* P3 at the start means that its using ascii colours. You then have to specify 
-* the size of the image, and the colour space it's using (in our case, 255)
-* and then you can start using RGB triplets to state the colour of each pixel
-*
-* This file needs to be run using at minimum cpp11, and the output is 
-* redirected into `image.ppm`, which can be opened in any image viewer. 
-*/
+ * A PPM file is a type of file that represents an image by specifying
+ * every line of pixels, as well as some data at the start that informs the
+ * renderer how to display the image.
+ *
+ * P3 at the start means that its using ascii colours. You then have to specify
+ * the size of the image, and the colour space it's using (in our case, 255)
+ * and then you can start using RGB triplets to state the colour of each pixel
+ *
+ * This file needs to be run using at minimum cpp11, and the output is
+ * redirected into `image.ppm`, which can be opened in any image viewer.
+ */
 
 int main() {
     // Create image
@@ -19,9 +19,9 @@ int main() {
     const int img_width = 256;
 
     // File renderer
-    std::cout << "P3\n" << img_height << ' ' << img_width << "\n255\n"; 
+    std::cout << "P3\n" << img_height << ' ' << img_width << "\n255\n";
 
-    for (int j = img_height-1; j >= 0; --j) {
+    for (int j = img_height - 1; j >= 0; --j) {
         // This is basically a progress indicator
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 
