2be57e2181c02cc7e4cd2100e484eb97c6355e71
Ttibsi <ashisbitt@icloud.com>
2022-07-28 12:28:05 +0100

    Move main.cpp and write new headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+#include "color.h"
+#include "vec3.h"
+
 /*
  * A PPM file is a type of file that represents an image by specifying
  * every line of pixels, as well as some data at the start that informs the
@@ -13,14 +16,7 @@
  * redirected into `image.ppm`, which can be opened in any image viewer.
  */
 
-int main() {
-    // Create image
-    const int img_height = 256;
-    const int img_width = 256;
-
-    // File renderer
-    std::cout << "P3\n" << img_height << ' ' << img_width << "\n255\n";
-
+void basic_render(int img_width, int img_height) {
     for (int j = img_height - 1; j >= 0; --j) {
         // This is basically a progress indicator
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
@@ -36,6 +32,26 @@ int main() {
             int ib = static_cast<int>(255.999 * b);
 
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+}
+
+int main() {
+    // Create image
+    const int img_height = 256;
+    const int img_width = 256;
+
+    // File renderer
+    std::cout << "P3\n" << img_height << ' ' << img_width << "\n255\n";
+    //basic_render(img_width, img_height)
+
+    for (int j = img_height - 1; j >= 0; --j) {
+        // This is basically a progress indicator
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+
+        for (int i = 0; i < img_width; ++i) {
+            color pixel_color(double(i)/(img_width-1), double(j)/(img_height-1), 0.25);
+            write_color(std::cout, pixel_color);
         }
     }
 
--- src/vec3.h
+++ src/vec3.h
@@ -37,10 +37,10 @@ class vec3 {
             return *this;
         }
 
-        vec3& operator/= (const doubt t) { return *this *= 1/t; }
+        vec3& operator/= (const double t) { return *this *= 1/t; }
  
         // Method signatures
-        double length() const { return std:sqrt(length_squared()); }
+        double length() const { return std::sqrt(length_squared()); }
         double length_squared() const { 
             return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
         }
@@ -86,6 +86,6 @@ inline vec3 cross (const vec3 &u, const vec3 &v) {
                 u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
-inline vec3 unit_vector (vec3 v) { return v / v.lentth(); }
+inline vec3 unit_vector (vec3 v) { return v / v.length(); }
 
 #endif
