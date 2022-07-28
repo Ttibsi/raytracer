f28547cdc743683c358ce653d344ae0bc49e6476
Ttibsi <ashisbitt@icloud.com>
2022-07-28 15:28:11 +0100

    blue-white gradient

--- src/color.h
+++ src/color.h
@@ -1,8 +1,8 @@
 #ifndef COLOR_H
 #define COLOR_H
 
-#include <iostream>
 #include "vec3.h"
+#include <iostream>
 
 void write_color(std::ostream &out, color pixel_color) {
     out << static_cast<int>(255.999 * pixel_color.x()) << ' '
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "color.h"
+#include "ray.h"
 #include "vec3.h"
 
 /*
@@ -15,6 +16,12 @@
  * This file needs to be run using at minimum cpp11, and the output is
  * redirected into `image.ppm`, which can be opened in any image viewer.
  */
+
+color ray_color(const ray &r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
 
 void basic_render(int img_width, int img_height) {
     for (int j = img_height - 1; j >= 0; --j) {
@@ -38,19 +45,35 @@ void basic_render(int img_width, int img_height) {
 
 int main() {
     // Create image
-    const int img_height = 256;
-    const int img_width = 256;
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int img_width = 400;
+    const int img_height = static_cast<int>(img_width / aspect_ratio);
+
+    // Create camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner =
+        origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
     // File renderer
     std::cout << "P3\n" << img_height << ' ' << img_width << "\n255\n";
-    //basic_render(img_width, img_height)
+    // basic_render(img_width, img_height)
 
     for (int j = img_height - 1; j >= 0; --j) {
         // This is basically a progress indicator
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 
         for (int i = 0; i < img_width; ++i) {
-            color pixel_color(double(i)/(img_width-1), double(j)/(img_height-1), 0.25);
+            auto u = double(i) / (img_width - 1);
+            auto v = double(j) / (img_height - 1);
+            ray r(origin,
+                  lower_left_corner + u * horizontal + v * vertical - origin);
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
