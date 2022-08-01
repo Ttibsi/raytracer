b3bdafe300e751d501755ce56e9bfa9a87f931e8
Ttibsi <ashisbitt@icloud.com>
2022-08-01 16:24:19 +0100

    Handle multiple objects in scene

--- src/sphere.h
+++ src/sphere.h
@@ -4,13 +4,13 @@
 #include "hittable.h"
 #include "vec3.h"
 
-class Sphere : publc hittable {
+class Sphere : public hittable {
   public:
     point3 center;
     double radius;
 
-    Sphere(){};
-    Sphere(point3 cen, double r) : center{cen}, radius{r};
+    Sphere() {}
+    Sphere(point3 cen, double r) : center{cen}, radius{r} {}
 
     virtual bool hit(const ray &r, double t_min, double t_max,
                      hit_record &rec) const override;
