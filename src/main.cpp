34ab5c4964acf6c12ace4414b46f17113fcdbe21
Ttibsi <ashisbitt@icloud.com>
2022-08-02 17:14:41 +0100

    Refraction

--- src/main.cpp
+++ src/main.cpp
@@ -45,9 +45,9 @@ int main() {
     hittable_list world;
 
     auto material_ground = std::make_shared<Lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = std::make_shared<Lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left = std::make_shared<Metal>(color(0.8, 0.8, 0.8));
-    auto material_right = std::make_shared<Metal>(color(0.8, 0.6, 0.2));
+    auto material_center = std::make_shared<Lambertian>(color(0.1, 0.2, 0.5));
+    auto material_left = std::make_shared<Dielectric>(1.5);
+    auto material_right = std::make_shared<Metal>(color(0.8, 0.6, 0.2), 0.0);
 
     world.add(std::make_shared<Sphere>(point3(0.0, -100.5, -1.0), 100.0,
                                        material_ground));
