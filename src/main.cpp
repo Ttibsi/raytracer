1e5e6b45f43121416e6c37b487c3f3ba3b1ea6bd
Ttibsi <ashisbitt@icloud.com>
2022-08-03 12:55:11 +0100

    Lens approximation

--- src/main.cpp
+++ src/main.cpp
@@ -63,8 +63,15 @@ int main() {
     // Camera
     // You can change the coordinates in the vectors here to move the camera
     // around
-    Camera cam(point3(-2, 2, 1), point3(0, 0, -1), vec3(0, 1, 0), 20,
-               aspect_ratio);
+
+    point3 lookfrom(3, 3, 2);
+    point3 lookat(0, 0, -1);
+    vec3 vup(0, 1, 0);
+    auto dist_to_focus = (lookfrom - lookat).length();
+    auto aperture = 2.0;
+
+    Camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture,
+               dist_to_focus);
 
     // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
