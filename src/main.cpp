eef8986f12c6d83c724e1cc5ca7c221eecc4b8df
Ttibsi <ashisbitt@icloud.com>
2022-08-03 17:42:29 +0100

    Design a scene, but takes forever to render

--- src/main.cpp
+++ src/main.cpp
@@ -31,44 +31,97 @@ color ray_color(const ray &r, const hittable &world, int depth) {
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
+hittable_list scene() {
+    hittable_list world;
+
+    auto ground_material = std::make_shared<Lambertian>(color(0.5, 0.5, 0.5));
+    world.add(
+        std::make_shared<Sphere>(point3(0, -1000, 0), 1000, ground_material));
+
+    for (int a = -11; a < 11; ++a) {
+        for (int b = -11; b < 11; ++b) {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9 * random_double(), 0.2,
+                          b + 0.9 * random_double());
+
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                std::shared_ptr<Material> sphere_material;
+
+                if (choose_mat < 0.8) {
+                    auto albedo = color::random() * color::random();
+                    sphere_material = std::make_shared<Lambertian>(albedo);
+                    world.add(
+                        std::make_shared<Sphere>(center, 0.2, sphere_material));
+                } else if (choose_mat < 0.95) {
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = std::make_shared<Metal>(albedo, fuzz);
+                    world.add(
+                        std::make_shared<Sphere>(center, 0.2, sphere_material));
+                } else {
+                    sphere_material = std::make_shared<Dielectric>(1.5);
+                    world.add(
+                        std::make_shared<Sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = std::make_shared<Dielectric>(1.5);
+    world.add(std::make_shared<Sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = std::make_shared<Lambertian>(color(0.4, 0.2, 0.1));
+    world.add(std::make_shared<Sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = std::make_shared<Metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(std::make_shared<Sphere>(point3(4, 1, 0), 1.0, material3));
+
+    return world;
+}
+
 int main() {
 
     // Image
 
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 400;
+    const int image_width = 1900;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 100;
+    const int samples_per_pixel = 250;
     const int max_depth = 50;
 
     // World
-    hittable_list world;
-
-    auto material_ground = std::make_shared<Lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = std::make_shared<Lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left = std::make_shared<Dielectric>(1.5);
-    auto material_right = std::make_shared<Metal>(color(0.8, 0.6, 0.2), 0.0);
-
-    world.add(std::make_shared<Sphere>(point3(0.0, -100.5, -1.0), 100.0,
-                                       material_ground));
-    world.add(
-        std::make_shared<Sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
-    world.add(
-        std::make_shared<Sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
-    world.add(std::make_shared<Sphere>(point3(-1.0, 0.0, -1.0), -0.45,
-                                       material_left));
-    world.add(
-        std::make_shared<Sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
-
+    auto world = scene();
+    //    hittable_list world;
+    //
+    //    auto material_ground = std::make_shared<Lambertian>(color(0.8, 0.8,
+    //    0.0)); auto material_center = std::make_shared<Lambertian>(color(0.1,
+    //    0.2, 0.5)); auto material_left = std::make_shared<Dielectric>(1.5);
+    //    auto material_right = std::make_shared<Metal>(color(0.8, 0.6, 0.2),
+    //    0.0);
+    //
+    //    world.add(std::make_shared<Sphere>(point3(0.0, -100.5, -1.0), 100.0,
+    //                                       material_ground));
+    //    world.add(
+    //        std::make_shared<Sphere>(point3(0.0, 0.0, -1.0), 0.5,
+    //        material_center));
+    //    world.add(
+    //        std::make_shared<Sphere>(point3(-1.0, 0.0, -1.0), 0.5,
+    //        material_left));
+    //    world.add(std::make_shared<Sphere>(point3(-1.0, 0.0, -1.0), -0.45,
+    //                                       material_left));
+    //    world.add(
+    //        std::make_shared<Sphere>(point3(1.0, 0.0, -1.0), 0.5,
+    //        material_right));
+    //
     // Camera
     // You can change the coordinates in the vectors here to move the camera
     // around
 
-    point3 lookfrom(3, 3, 2);
-    point3 lookat(0, 0, -1);
+    point3 lookfrom(13, 2, 3);
+    point3 lookat(0, 0, 0);
     vec3 vup(0, 1, 0);
-    auto dist_to_focus = (lookfrom - lookat).length();
-    auto aperture = 2.0;
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.1;
 
     Camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture,
                dist_to_focus);
